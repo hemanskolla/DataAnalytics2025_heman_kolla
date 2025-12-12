3057e16e7b35c05dc452a4ff080d2d2d38b825b3
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2025-12-12 06:25:04 -0500

    Setup proj and Lab3 directories

---
+++ proj/proj.R
@@ -0,0 +1,10 @@
+# ------------------- IMPORTS -------------------
+
+library(readr)
+
+# ------------------- LOAD DATA -------------------
+
+setwd("C:\\Users\\kollah\\OneDrive - Rensselaer Polytechnic Institute\\Documents\\DataAnalytics2025_heman_kolla\\proj")
+df <- read_csv("C:\\Users\\kollah\\OneDrive - Rensselaer Polytechnic Institute\\Documents\\QML_URP_F25\\data.csv")
+
+# ------------------- DATA CLEANING -------------------
